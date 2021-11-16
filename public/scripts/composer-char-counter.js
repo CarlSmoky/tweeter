aa07b72a718536fa62416f3fadb49e92392db309
tsumitakaoru <k.tsumita01@gmail.com>
2021-11-16 15:53:43 -0500

    Implement the character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,31 +1,16 @@
 $(document).ready(function () {
-  console.log("hello from the script file");
-  // const $element = $(".title");
-  // console.log($element);
   const $tweetText = $("#tweet-text");
-  // console.log($tweetText);
-  // $tweetText.on('blur', () => {
-  //   console.log('this textarea was blured');
-  // });
-  // $tweetText.on('keydown', () => {
-  //   console.log('this textarea was keydown');
-  // });
-  $tweetText.on('keyup', () => {
-    console.log('this textarea was keyup');
+  $tweetText.keyup(function () {
+    const max = 140;
+    const characterCount = $(this).val().length;
+    const displayCount = max - characterCount;
+    const $counter = $(".counter");
+    $counter.text(displayCount);
+    if (characterCount > max) {
+      $counter.addClass("alert");
+    }
+    if (characterCount < max) {
+      $counter.removeClass("alert");
+    }
   });
-  // $tweetText.on('keypress', () => {
-  //   console.log('this textarea was keypress');
-  // });
-  // $tweetText.on('change ', () => {
-  //   console.log('this textarea was change ');
-  // });
-  // $tweetText.on('input', () => {
-  //   console.log('this textarea was input');
-  // });
 });
-
-// document.addEventListener("click", () => {
-//   // eslint-disable-next-line no-undef
-//   alert("You clicked somewhere on the page!");
-// });
-
