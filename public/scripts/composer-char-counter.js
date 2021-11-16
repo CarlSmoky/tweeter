062ceef2b59275985c526c42600be9ec4bd805af
tsumitakaoru <k.tsumita01@gmail.com>
2021-11-16 15:51:13 -0500

    Add test events

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log("hello from the script file");
-  const $element = $(".title");
-  console.log($element);
-  $element.on('click', () => {
-    console.log('this title was clicked');
+  // const $element = $(".title");
+  // console.log($element);
+  const $tweetText = $("#tweet-text");
+  // console.log($tweetText);
+  // $tweetText.on('blur', () => {
+  //   console.log('this textarea was blured');
+  // });
+  // $tweetText.on('keydown', () => {
+  //   console.log('this textarea was keydown');
+  // });
+  $tweetText.on('keyup', () => {
+    console.log('this textarea was keyup');
   });
+  // $tweetText.on('keypress', () => {
+  //   console.log('this textarea was keypress');
+  // });
+  // $tweetText.on('change ', () => {
+  //   console.log('this textarea was change ');
+  // });
+  // $tweetText.on('input', () => {
+  //   console.log('this textarea was input');
+  // });
 });
 
 // document.addEventListener("click", () => {
