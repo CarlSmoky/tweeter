3b467a73deba9fe08349bb20d8e0d6dee97699a2
tsumitakaoru <k.tsumita01@gmail.com>
2021-11-18 05:41:46 -0500

    Add error display

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -84,6 +84,20 @@ $(document).ready(function () {
     $('.errorContainer').empty();
     let url = "/tweets";
     
+    const val = $("#tweet-text").val();
+    console.log(val);
+    if (val === "" || !val) {
+      $('.errorContainer').empty();
+      const errorMsg = '<p class="errorMsg"><i class="fas fa-exclamation-triangle"></i>You need input something.<i class="fas fa-exclamation-triangle"></i></p>';
+      $('.errorContainer').append(errorMsg);
+      return;
+    }
+    if (val.length > 140) {
+      $('.errorContainer').empty();
+      const errorMsg = '<p class="errorMsg"><i class="fas fa-exclamation-triangle"></i>Too long!! You can input 140 letters maximum.<i class="fas fa-exclamation-triangle"></i></p>';
+      $('.errorContainer').append(errorMsg);
+      return;
+    }
     let tweet = $("#tweet-text");
 
     const tweetText = tweet.serialize();
